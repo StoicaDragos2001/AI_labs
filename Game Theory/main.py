2250d207ec8f464759cddc92f936448e2a6b4a65
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2022-10-31 14:12:27 +0200

    Update main.py

--- Game Theory/main.py
+++ Game Theory/main.py
@@ -1,9 +1,59 @@
 import pandas as pd
-from collections import Counter
+
 
 def read_from_file():
     df = pd.read_csv("game.csv", index_col=0)
     return df.to_dict("dict")
+
+
+def dominant_strategy(player, dict_for_player):
+    if player == 0:
+        player += 1
+        ok = 0
+    elif player == 1:
+        player -= 1
+        ok = 1
+    dict_max_moves_columns = {}
+    for i in dict_for_player:
+        if i[player] not in dict_max_moves_columns or dict_max_moves_columns[i[player]] < dict_for_player[i]:
+            dict_max_moves_columns[i[player]] = dict_for_player[i]
+    max_values_for_player = []
+    for i in dict_for_player:
+        if i[player] in dict_max_moves_columns and dict_for_player[i] == dict_max_moves_columns[i[player]]:
+            max_values_for_player.append(i[ok])
+    dict_with_counts = {}
+    for i in set(max_values_for_player):
+        dict_with_counts[i] = max_values_for_player.count(i)
+    dominant_value_for_player = [i for i in dict_with_counts if
+                                 dict_with_counts[i] == max(list(dict_with_counts.values()))]
+    if len(dominant_value_for_player) == 1:
+        print(f"Dominant strategy for player {'B' if ok else 'A'} is {dominant_value_for_player}")
+    else:
+        print("Dominant strategy doesn't exist")
+
+
+def nash_equilibrum(dict_player_A, dict_player_B):
+    dict_max_moves_rows_A = {}
+    for i in dict_player_A:
+        if i[0] not in dict_max_moves_rows_A or dict_max_moves_rows_A[i[0]] < dict_player_A[i]:
+            dict_max_moves_rows_A[i[0]] = dict_player_A[i]
+    dict_max_moves_rows_B = {}
+    for i in dict_player_B:
+        if i[1] not in dict_max_moves_rows_B or dict_max_moves_rows_B[i[1]] < dict_player_B[i]:
+            dict_max_moves_rows_B[i[1]] = dict_player_B[i]
+    list_max_states_A = []
+    for i in dict_player_A:
+        if i[0] in dict_max_moves_rows_A and dict_player_A[i] == dict_max_moves_rows_A[i[0]]:
+            list_max_states_A.append(i)
+    list_max_states_B = []
+    for i in dict_player_B:
+        if i[1] in dict_max_moves_rows_B and dict_player_B[i] == dict_max_moves_rows_B[i[1]]:
+            list_max_states_B.append(i)
+    solution = set(list_max_states_A).intersection(set(list_max_states_B))
+    if len(solution) > 0:
+        print(solution, " represents a Nash equilibrium")
+    else:
+        print("Nash equilibrium doesn't exist")
 
 
 if __name__ == '__main__':
@@ -17,17 +67,6 @@ if __name__ == '__main__':
             dict_B[(intern_key, key)] = splitting[1]
     print(f"Player A: {dict_A}")
     print(f"Player B: {dict_B}")
-    max_values_for_A = [i for i in dict_A if dict_A[i] == max(list(dict_A.values()))]
-    counter_max_values_A = dict(Counter(elem[1] for elem in max_values_for_A))
-    dominant_strategy_A = [i for i in counter_max_values_A if
-                           counter_max_values_A[i] == max(list(counter_max_values_A.values()))]
-    print(dominant_strategy_A, " represents a dominant strategy for player A")
-    max_values_for_B = [i for i in dict_B if dict_B[i] == max(list(dict_B.values()))]
-    counter_max_values_B=dict(Counter(elem[1] for elem in max_values_for_B))
-    dominant_strategy_B=[i for i in counter_max_values_B if counter_max_values_B[i] == max(list(counter_max_values_B.values()))]
-    print(dominant_strategy_B, " represents a dominant strategy for player B")
-    solution = set(max_values_for_A).intersection(set(max_values_for_B))
-    if len(solution) > 0:
-        print(solution, " represents a Nash equilibrium")
-    else:
-        print("Nash equilibrium doesn't exist")
+    dominant_strategy(0, dict_A)
+    dominant_strategy(1, dict_B)
+    nash_equilibrum(dict_A, dict_B)
